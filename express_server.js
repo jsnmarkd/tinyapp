8f75da3f1aacdc2d513e51dd88bafb15cb55a77e
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-23 18:23:43 -0500

    Placed the urls_new above the urls_show

--- express_server.js
+++ express_server.js
@@ -18,13 +18,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
 });
 
 app.get("/urls.json", (req, res) => {
