d006d7dadaa129abaa66cf051d00d30763fb969d
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-23 18:49:52 -0500

    Integrated POST requests and a POST route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -9,6 +9,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
