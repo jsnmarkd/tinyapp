ab30d630f971d87f57041cf7c2378ea11b134ab1
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-23 20:11:04 -0500

    Function that generates 6 random alphanumerical string works

--- express_server.js
+++ express_server.js
@@ -10,7 +10,19 @@ const urlDatabase = {
 };
 
 function generateRandomString() { 
-  
+  let result = '';
+  const arrayOfLetters = 
+    [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+      '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+    ];
+  for (let i = 6; i > 0; i--) {
+    result += arrayOfLetters[Math.floor(Math.random() * arrayOfLetters.length)];
+  }
+  return result;
 };
 
 app.use(express.urlencoded({ extended: true }));
