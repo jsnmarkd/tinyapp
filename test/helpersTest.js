18b6cef9b6298c47b10cc7256fd7761a4b9841d2
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-26 19:36:31 -0500

    Installed M&C and implemented test file to test helper function

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert(user.id === expectedUserID, "Should return the expected user ID");
+  });
+  it('should return null if an email is not in our users database', function() {
+    const user = getUserByEmail("nope@example.com", testUsers)
+    const expectedUserID = null;
+    assert(user === expectedUserID, "Should return undefined");
+  });
+});
